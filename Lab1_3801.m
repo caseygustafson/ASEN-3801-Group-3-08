5df3843cfc04634af8794b6d55b38f50a0e02d49
ieamodio <isam5535@colorado.edu>
2025-08-26 16:03:11 -0600

    Add files via upload

---
+++ Lab1_3801.m
@@ -0,0 +1,18 @@
+% Contributors: Isabelle Amodio
+% Course number: ASEN 3801
+% File name: AirRelativeVelocityVectorToWindAngles
+% Created: 8/26/25
+
+%% Parameters 
+tspan=[0,20];
+tol=1e-8;
+
+%% Set Up System of Equations
+w=a(:,1);
+x=a(:,2);
+y=a(:,3);
+z=a(:,4);
+
+
+
+
