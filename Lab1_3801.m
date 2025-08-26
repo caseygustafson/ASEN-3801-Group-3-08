93e2bc57e147a92b2f08a3bbb61f6e1eaf0d6224
ieamodio <isam5535@colorado.edu>
2025-08-26 16:53:13 -0600

    Add files via upload

--- Lab1_3801.m
+++ Lab1_3801.m
@@ -1,11 +1,20 @@
+clear all 
+clc
+
 % Contributors: Isabelle Amodio
 % Course number: ASEN 3801
 % File name: AirRelativeVelocityVectorToWindAngles
 % Created: 8/26/25
 
+tol=[1e-2,1e-4,1e-6,1e-8,1e-10]'; % tolerance values 
+
+for i=1:length(tol)
+    ODEsolver(tol(i));
+end
+
+function ODEsolver(tol)
 %% Parameters 
 tspan=[0,20]';
-tol=[1e-2,1e-4,1e-6,1e-8,1e-10]';
 state0=[1; 0.5; -0.2; 0.3]';  % initial conditions (non-zero values)
 %state=[w,x,y,z];
 
@@ -24,7 +33,7 @@ subplot(4,1,1)
 plot(t,w,'LineWidth',1.5);
 grid on
 ylabel('w (n.d)')
-title('Solution Over 0-20s')
+
 
 % subplot for x
 subplot(4,1,2)
@@ -43,7 +52,7 @@ subplot(4,1,4)
 plot(t,z,'LineWidth',1.5);
 grid on
 ylabel('z (n.d)')
-xlabel('Time (s)')
+xlabel('Time (n.d)')
 
 
 %% function for dot values
@@ -60,5 +69,5 @@ z_dot=(x*y)-(y^2)-(3*z*z*z);
 
 dot_value=[w_dot,x_dot,y_dot,z_dot]';
 end
-
+end
 
